681e1002dbf8e9c72c6721dee732839dfe239cb9
oshiromi <85414986+oshiromi@users.noreply.github.com>
2022-10-08 18:45:48 +0200

    retrieved API data

--- src/data/fetch_api_data.py
+++ src/data/fetch_api_data.py
@@ -1,5 +1,16 @@
+import requests
+import os
+from dotenv import find_dotenv, load_dotenv
 
+load_dotenv(find_dotenv())
+API_KEY = os.environ.get('API_KEY')
 
+url = "https://free-nba.p.rapidapi.com/games"
+headers = {"X-RapidAPI-Key": API_KEY,
+           "X-RapidAPI-Host": "free-nba.p.rapidapi.com"}
+params = {"page": "0",
+          "per_page": "1000"}
 
+response = requests.get(url, headers=headers, params=params)
 
-if __name__ == '__main__':
+print(response.content)
